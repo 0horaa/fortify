0d62d6444134859ec6cc4404553c3f871e186bfb
Rômulo Lima <romulo.lf123@gmail.com>
2022-10-08 11:17:04 -0300

    Switch case refactor

--- pages/home-academy.php
+++ pages/home-academy.php
@@ -2,17 +2,23 @@
     include_once("includes-academy/header-academy.php");
     if(isset($_GET["pg"])){
         $parameter = $_GET["pg"];
-        if($parameter === "home"){
-            include_once("includes-academy/main-academy.php");
-        }else if($parameter === "edit"){
-            include_once("includes-academy/update-academy.php");
-        }else if($parameter === "search"){
-            include_once("includes-academy/search-academy.php");
-        }else{
-            include_once("includes-academy/main-academy.php");
+        
+        switch($parameter){
+            case 'home':
+                include_once("includes-academy/main-academy.php");
+                break;
+            case 'edit':
+                include_once("includes-academy/update-academy.php");
+                break;
+            case 'search':
+                include_once("includes-academy/search-academy.php");
+                break;
+            default:
+                include_once("includes-academy/main-academy.php");
         }
     }else{
         include_once("includes-academy/main-academy.php");
     }
+
     include_once("includes-academy/footer-academy.php");
 ?>
